f4a5ceaa97032c651e6d28cd67d0aeb5dd07ea80
Nora Dahlberg <ndahlberg95@gmail.com>
2021-04-22 09:37:03 -0600

    install npm, mysql, sequelize, and dotenv

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize('e_commerce_db', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;
+
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS e_commerce_db;
+
+CREATE DATABASE e_commerce_db;
