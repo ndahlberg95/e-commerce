7937c8791ccbc2126c468297382a6231ffdccdec
Nora Dahlberg <ndahlberg95@gmail.com>
2021-04-24 16:34:23 -0600

    add sql schema

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 const categoryRoutes = require('./category-routes.js');
-
 router.use('/categories', categoryRoutes);
+
+const productRoutes = require('./product-routes.js');
+router.use('/products', productRoutes);
+
+const productTagRoutes = require('./product-tag-routes.js');
+router.use('/product-tags', productTagRoutes);
+
+const tagRoutes = require('./tag-routes.js');
+router.use('/tags', tagRoutes);
 
 module.exports = router;
